67e3b1392fedd5104644ec6be1f0d06b4e62e935
Luc Patiny <luc@patiny.com>
2021-08-24 12:56:12 +0200

    chore: remove uint8 conversion and win 5% speed

--- src/parser.js
+++ src/parser.js
@@ -11,8 +11,6 @@ exports.parse = function (xmlData, options, validationOption) {
     xmlbuffer2xmlnode.defaultOptions,
     xmlbuffer2xmlnode.props,
   );
-  // todo: Should delete following line
-  xmlData = new Uint8Array(xmlData); //ensures that the data is valid in case of fs file reading
   const traversableObj = xmlbuffer2xmlnode.getTraversalObj(xmlData, options);
   //print(traversableObj, "  ");
   return nodeToJson.convertToJson(traversableObj, options);
