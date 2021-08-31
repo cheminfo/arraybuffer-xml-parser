1780b811040a63581033ec72c3d596ba68b7a7c2
Luc Patiny <luc@patiny.com>
2021-08-31 06:49:05 +0200

    chore: add benchmark folder

---
+++ benchmark/small.js
@@ -0,0 +1,19 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const data = readFileSync(join(__dirname, 'small.xml'));
+const { parse } = require('../lib/index.js');
+
+console.time('start');
+const result = parse(data, {
+  parseAttributeValue: false,
+  ignoreAttributes: false,
+  parseNodeValue: false,
+  tagValueProcessor: (value, name) => {
+    return value;
+  },
+});
+console.timeEnd('start');
+//console.log(
+//  result.indexedmzML.mzML.run.spectrumList.spectrum[1].binaryDataArrayList,
+//);
