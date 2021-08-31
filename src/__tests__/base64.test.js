cdcbd473419aaf3b9409a0a9c5b04cb727b9b1e7
Luc Patiny <luc@patiny.com>
2021-08-31 08:23:16 +0200

    test: add base64 test

---
+++ src/__tests__/base64.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-tabs */
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { parse } from '../parse';
+
+// library to convert base64 <--> arrayBuffer: https://github.com/niklasvh/base64-arraybuffer/blob/master/src/index.ts
+
+test('base64 parsing', function () {
+  const xmlData = readFileSync(join(__dirname, 'assets', 'base64.xml'));
+
+  let result = parse(xmlData, {
+    ignoreAttributes: false,
+  });
+
+  console.log(result);
+});
