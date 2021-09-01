8ffdf50cbef1713ffd381c61dd2b00f93f148820
Luc Patiny <luc@patiny.com>
2021-09-01 15:13:21 +0200

    docs: add jsdoc

--- src/parse.js
+++ src/parse.js
@@ -2,6 +2,27 @@ import { defaultOptions } from './traversable/defaultOptions';
 import { getTraversable } from './traversable/getTraversable';
 import { traversableToJSON } from './traversableToJSON';
 
+/**
+ *
+ * @param {ArrayBuffer|Uint8Arra} xmlData
+ * @param {object} [options={}]
+ * @param {string} [attributeNamePrefix='@_']
+ * @param {boolean} [attributesNodeName=false]
+ * @param {string} [textNodeName='#text']
+ * @param {boolean} [trimValues=true] should we remove ascii < 32
+ * @param {boolean} [ignoreAttributes=false] skip attributes
+ * @param {boolean} [ignoreNameSpace=false]
+ * @param {boolean} [dynamicTypingAttributeValue=true] Parse attribute values that looks like number or boolean
+ * @param {boolean} [allowBooleanAttributes=false]
+ * @param {boolean} [dynamicTypingNodeValue=true] Parse tag values that looks like number or boolean
+ * @param {boolean} [arrayMode=false]
+ * @param {boolean} [cdataTagName=false]
+ * @param {function} [tagValueProcessor=(v, node) => decoder.decode(v)] Tag values can be modified during parsing. By default we decode the tag value (a Uint8Array) using TextDecoder
+ * @param {function} [attributeValueProcessor=(v) => v] Attribute values can be modified during parsing
+ * @param {boolean} [stopNodes=[]] prevent further parsing
+ *
+ * @returns {object}
+ */
 export function parse(xmlData, options = {}) {
   if (typeof xmlData === 'string') {
     const encoder = new TextEncoder();
